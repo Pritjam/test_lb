639779cf0a885b4bf3ba768a951a53ed8e14e4f7
Prithvi Jamadagni <pritjam@gmail.com>
2024-08-20 19:01:41 -0500

    added a .o file as well

--- src/hello.c
+++ src/hello.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
-#include <time.h>
-int main(){printf("This is the LB Lab Hello World Program!\n");printf("Following this is a series of magic numbers.\n");time_t t=time(NULL);unsigned long a=0,b=1;for(int i=0;i<94;i++){printf("%lu\n", a^t);b+=a;a=b-a;}}
+extern long key;
+
+int main() {
+  printf("This is the LB Lab Hello World Program!\n");
+  printf("Following this is a series of magic numbers.\n");
+  unsigned long a = 0, b = 1;
+  for (int i = 0; i < 94; i++) {
+    printf("%lu\n", a ^ key);
+    b += a;
+    a = b - a;
+  }
+}
